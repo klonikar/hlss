664b7e804d649d2471353dcd7f7756c60f872d5c
kiran <kiran@allgovision.com>
2018-02-06 16:11:57 +0530

    changed to start ffmpeg process and use its stdout to start the stream

--- examples/live.js
+++ examples/live.js
@@ -1,20 +1,23 @@
+#!node
+
 'use strict';
 
-const hlss = require('../index.js');
+const { spawn } = require('child_process');
+const fs = require("fs")
+const hlss = require('hlss');
 
-const args = process.argv;
-
-if (args.length !== 3) {
-  console.log('Usage:\n\
-  ffmpeg -re -i source.mp4 -c copy -bsf h264_mp4toannexb\
- -f mpegts - | node live.js out_path');
-  process.exit(1);
+if(process.argv.length < 4) {
+    console.log("Usage: " + process.argv[1] + " <cameraURL> <outputFolder>");
+    process.exit(0)
 }
 
-const stream = process.stdin;
+// "rtsp://root:pass@192.168.10.49:554/axis-media/media.amp?resolution=640X480&fps=15"
+const ffmpeg = spawn("ffmpeg", ['-re', '-i', process.argv[2],  '-an', '-c', 'copy', '-bsf', 'h264_mp4toannexb', '-f', 'mpegts', '-'], 
+	                  {stdio: ['ignore', 'pipe', 'ignore']});
+console.log("ffmpeg id:", ffmpeg.pid);
 
 const segmenter = new hlss({
-  outPath: args[2],
+  outPath: process.argv[3],
   streamName: 'test',
   segDuration: 5,
   segNumber: 4,
@@ -25,4 +28,31 @@ segmenter.on('done', () => {
   console.log('all done!');
 });
 
-segmenter.start(stream);
+segmenter.start(ffmpeg.stdout);
+
+if(ffmpeg.stderr) {
+	ffmpeg.stderr.on('data', (data) => {
+		console.log(`ffmpeg stderr: ${data}`);
+	});
+}
+
+ffmpeg.on('close', (code) => {
+    if(code != 0) {
+    	console.log(`ffmpeg process exited (close) with code ${code}`);
+    }
+    process.exit(0);
+});
+
+ffmpeg.on('error', (code) => {
+    if(code != 0) {
+    	console.log(`ffmpeg process exited (error) with code ${code}`);
+    }
+    process.exit(0);
+});
+
+ffmpeg.on('exit', (code) => {
+    if(code != 0) {
+    	console.log(`ffmpeg process exited with code ${code}`);
+    }
+    process.exit(0);
+});
